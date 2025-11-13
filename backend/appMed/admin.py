bee51a7772b28c0f31a0d4fbc8a5374009f3a825
joaocorreasilvajcs <juguirald@gmail.com>
2025-11-13 14:58:01 -0300

    feat(backend): implementar JWT, escopo por responsável, paginação/throttling e migrações (email único, timestamps)

--- backend/appMed/admin.py
+++ backend/appMed/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from .models import Registro, RegistroMed
+from .models import Registro, Idoso, Medicamento, Horario, Tomada
 
-admin.site.register(Registro)
-admin.site.register(RegistroMed)
+
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "email")
+    search_fields = ("nome", "email")
+
+
+class HorarioInline(admin.TabularInline):
+    model = Horario
+    extra = 0
+
+
+@admin.register(Idoso)
+class IdosoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "responsavel", "criado_em")
+    list_filter = ("responsavel",)
+    search_fields = ("nome",)
+
+
+@admin.register(Medicamento)
+class MedicamentoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "idoso", "frequencia_tipo", "ativo")
+    list_filter = ("frequencia_tipo", "ativo")
+    search_fields = ("nome", "idoso__nome")
+    inlines = [HorarioInline]
+
+
+@admin.register(Tomada)
+class TomadaAdmin(admin.ModelAdmin):
+    list_display = ("id", "idoso", "medicamento", "horario_previsto", "tomado")
+    list_filter = ("tomado", "medicamento")
+    search_fields = ("idoso__nome", "medicamento__nome")
